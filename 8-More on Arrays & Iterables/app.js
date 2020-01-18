0a35f8d41f6077ea3057f0bd9dde94e76f0cda45
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-01-18 18:38:14 +0200

    More-Arrays-v2

--- 8-More on Arrays & Iterables/app.js
+++ 8-More on Arrays & Iterables/app.js
@@ -1,77 +1,85 @@
-
 // Creating Array
 ///////////////////////
 const numbers = [1, 2, 3];
 console.log(numbers);
 
+//using construct array
 const moreNumbers = new Array(5); //[]
 console.log(moreNumbers);
 
-const yetMoreNumbers = Array.of(1, 2);
+const yetMoreNumbers = Array.of(1, 2); // of(pass generate numbers)
 console.log(yetMoreNumbers);
 
 const iterablesArray = Array.from([1, 2, 3]);
-const iterablesArray = Array.from('Hi!');
+const iterablesArray = Array.from("Hi!"); //split ['h' ,'i' ,'!']
 console.log(iterablesArray);
 
-const listItems = document.querySelectorAll('li');
+const listItems = document.querySelectorAll("li");
 console.log(listItems); //NodeList =>array Like Object "iterables"
-const arraylistItems = Array.from(listItems);
-console.log(arraylistItems); // NodeList Like Array
+const arrayListItems = Array.from(listItems); // convert NodeList to Array
+console.log(arrayListItems); // NodeList Like Array
 
-/////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Which Data Can You Store In Arrays?
+//////////////////////////////////////////
 
 const hobbies = ["sport", "eating"];
-const personalData = [20, 'Max', { moreDetails: [] }];
-const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+const personalData = [20, "Max", { moreDetails: [] }];
+const analyticsData = [
+  [1, 1.6],
+  [-5.4, 2.1]
+];
 for (data of analyticsData) {
-    for (const dataPoints of data) {
-        console.log('datapoint =>' + dataPoints)
-    }
+  for (const dataPoints of data) {
+    console.log("dataPoint =>" + dataPoints); // print number of multi dimension
+  }
 }
-///////////////////////////////////////////////////////////////
+console.log(personalData[1]); //Max
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// push(), pop(), unshift(), shift() - Adding & Removing Elements
+/////////////////////////////////////////////////////////////////
 
-const hobbies = ['Sports', 'Cooking'];
-hobbies.push("Reading"); // added element at end array 
-hobbies.unshift("Coding");// added element at start array 
-const poppedValue = hobbies.pop();  //remove element in end array
-hobbies.shift() //remove element at start array 
+const hobbies = ["Sports", "Cooking"];
+hobbies.push("Reading"); // added element at end array
+hobbies.unshift("Coding"); // added element at start array
+const poppedValue = hobbies.pop(); //remove element in end array
+hobbies.shift(); //remove element at start array
 console.log(hobbies);
 
-////////////////////////////////////////////////////////////////
-
-
-const hobbies = ['Sports', 'Cooking'];
-hobbies[1] = 'Coding';
+const hobbies = ["Sports", "Cooking"];
+hobbies[1] = "Coding";
 console.log(hobbies);
-hobbies[5] = "Reading"; //rarely used
+hobbies[5] = "Reading"; // empty X3  rarely used
 console.log(hobbies, hobbies[4]); //undefined
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  The splice() Method
+/////////////////////////////////////////
 
-hobbies.splice(0, 0, 'Good Food') //index=0 ,notdelte anyone =0
-console.log(hobbies); // GoodFood in firstelement
-hobbies.splice(1, 0, 'Good Food'); // shifted the secound element
-console.log(hobbies); //  Good Food in secound element
+hobbies.splice(0, 0, "Good Food"); //index=0 ,Not Delete anyone = 0
+console.log(hobbies); // GoodFood will be in first element in array
+hobbies.splice(1, 0, "Good Food"); // shifted the second element
+console.log(hobbies); //  Good Food will be  in second element
 
 const removeElements = hobbies.splice(0, 1); //removing the element[0]  , 1 => one element
-console.log(hobbies);
+console.log(hobbies); // sporting is deleted
 hobbies.splice(-1, 1);
-hobbies.splice(0); //removing all array 
+hobbies.splice(0); //removing all array
 
 console.log(hobbies);
-///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Selecting Ranges & Creating Copies with slice()
 ////////////////////////////////////////////////////////
 
 const testResult = [1, 5.3, 1.5, 10.99, -5, 10];
 console.log(testResult.slice()); // make copy of array
-const storeResults = testResult; //pointer ref to the original array
+const storeResults = testResult; //pointer ref to the original array "the same address"
 // testResult.push(5.91);
 console.log(storeResults, testResult);
 
-const storeResults = testResult.slice(); // copy of orignial array
-const storeResults = testResult.slice(0, 2); //start include end no [1,5.3]
-const storeResults = testResult.slice(-3, -1); //[10.99 ,-5]
-const storeResults = testResult.slice(1.5);
+const storeResults = testResult.slice(); // copy of original array
+const storeResults = testResult.slice(0, 2); //start include end no index 2   [1 , 5.3]
+const storeResults = testResult.slice(-3, -1); // [10.99 ,-5]
+const storeResults = testResult.slice(1.5); //[1.5, 10.99, -5, 10]
 testResult.push(5.91);
 console.log(storeResults, testResult);
 
@@ -80,42 +88,44 @@ console.log(storeResults, testResult);
 /////////////////////////////////////////
 
 const concatResults = testResult.concat([3.99, 2]); //concat 3.99 ,2 in result array and copy new array
-console.log(concatResults);
+console.log(concatResults); //  [1, 5.3, 1.5, 10.99, -5, 10,3.99, 2];
 
-////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Retrieving Indexes with indexOf() /& lastIndexOf()
 ///////////////////////////////////////////////////////
 
 const testResult = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-console.log(testResult.indexOf(1.5)); //2 =>start searching form left 
-console.log(testResult.lastIndexOf(1.5))//4 =>start searching form right
+console.log(testResult.indexOf(1.5)); //2 =>start searching form left
+console.log(testResult.lastIndexOf(1.5)); //4 =>start searching form right
 
-const personalData = [{ name: 'Max' }, { name: 'Menual' }];
-// object are referance
-console.log(personalData.indexOf({ name: 'Menual' }));//-1 codunt find any entry
+const personalData = [{ name: "Max" }, { name: "Manuel" }];
+// object are reference
+console.log(personalData.indexOf({ name: "Manuel" })); //-1 didn't find any entry = -1
+// Because object is reference value
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Finding Stuff: find() and findIndex()
 ///////////////////////////////////////////////////////
 
-const personalData = [{ name: 'Max' }, { name: 'Menual' }];
+const personalData = [{ name: "Max" }, { name: "Manuel" }];
 const manuel = personalData.find((person, index, persons) => {
-    return personalData.name === 'Menual';
+  return person.name === "Manuel";
 }); // the same object
-console.log(manuel); //{name:'Menual'}
-manuel.name = 'Anna';
-console.log(manuel, personalData);
+console.log(manuel); //{name:'Manuel'}
+manuel.name = "Anna"; // change value in object manuel
+console.log(manuel, personalData); // same abject will changed
 const maxIndex = personalData.findIndex((person, index, persons) => {
-    return personalData.name === 'Max';
+  return person.name === "Max";
 });
-console.log(maxIndex); //0
+console.log(maxIndex); // 0
 
 /////////////////////////////////////////////////////////////////////////////
 // Is it Included?
 ////////////////////////
 const testResult = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-console.log(testResult.includes(10.99)); //==
-console.log(testResult.indexOf(10.99) !== -1);
+console.log(testResult.includes(10.99)); // true
+// The Same fun includes equal condition in indexOf function
+console.log(testResult.indexOf(10.99) !== -1); //true
 
 /////////////////////////////////////////////////////////////////////////////
 // Alternative to for Loops: The forEach() Method
@@ -124,11 +134,14 @@ const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 const taxAdjustedPrices = [];
 // for (const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));   
+//     taxAdjustedPrices.push(price * (1 + tax));
 // }
+
+// using when transform to new  array created
 prices.forEach((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
-    taxAdjustedPrices.push(price * (1 + tax));
+  const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
+  // taxAdjustedPrices.push(price * (1 + tax));
+  taxAdjustedPrices.push(priceObj);
 });
 console.log(taxAdjustedPrices);
 
@@ -138,9 +151,10 @@ console.log(taxAdjustedPrices);
 
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
+// transform every element in prices array to priceObj
 const taxAdjustedPrices = prices.map((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
-    return priceObj;
+  const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
+  return priceObj;
 }); // map must be return something
 console.log(taxAdjustedPrices);
 
@@ -148,15 +162,27 @@ console.log(taxAdjustedPrices);
 //  sort()ing and reverse()ing
 ////////////////////////////////
 const prices = [10.99, 5.99, 3.99, 6.59];
+// "Only first Character is compared by default  10 > 3 but 1 < 3"
+console.log(prices.sort()); // convert every thing in string  [10.99 , 3.99 , 5.99 , 6.59]
+
 const storedPrices = prices.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a === b) {
-        return 0;
-    } else {
-        return -1;
-    }
-}); // converting any elemnt to string
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+}); // converting any element to string
+function reverseLogic(a, b) {
+  if (a > b) {
+    return -1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
 console.log(storedPrices);
 console.log(storedPrices.reverse());
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -164,62 +190,80 @@ console.log(storedPrices.reverse());
 ///////////////////////////////////////
 
 const filteredArray = prices.filter((price, index, prices) => {
-    return price > 6;
-})
+  return price > 6;
+});
 console.log(filteredArray);
 ////////////////////////////////////////////////////////////////////////////
 //   Where Arrow Functions Shine!
 ////////////////////////////////////
 const filteredArray = prices.filter(price => price > 6);
-console.log(filteredArray);
+console.log(filteredArray); // new Array has filtered price >6
 //////////////////////////////////////////////////////////////////////////////////////
 // The Important reduce() Method ??
 //////////////////////////////////
+// sum = 0;
+// prices.forEach((price) => {
+//     sum += price;
+// })
+// console.log(sum);
+
 const sum = prices.reduce((preValue, curValue, curIndex, prices) => {
-    return preValue + curValue;
+  return preValue + curValue;
 }, 0);
-console.log(sum);
+console.log(sum); // Look image reduce
 ////////////////////////////////////////////////////////////////////////
 //  Arrays & Strings - split() and join()
 //////////////////////////////////////////////
-const data = 'newyork;100.5;49';
-const transformedData = data.split(';');
-transformedData[1] = +transformedData[1];
+const data = "new york;100.5;49";
+const transformedData = data.split(";"); // ['new york' ,100.5 ,49]
+transformedData[1] = +transformedData[1]; //transform to real number
 console.log(transformedData);
-const nameFregements = ['zizo', 'fozy'];
-const name = nameFregements.join();
-const name = nameFregements.join(' ');
-console.log(nameFregements);
+const nameFragments = ["zizo", "fozy"];
+const name = nameFragments.join(); //zizo,fozy
+const name = nameFragments.join(" ");
+console.log(nameFragments);
 //////////////////////////////////////////////////////////////////////////////////
 //  The Spread Operator (...)
 ////////////////////////////////////
-const copiedNameFregements = [...nameFregements];
-nameFregements.push("MR");
-console.log(nameFregements, copiedNameFregements);
+const copiedNameFragments = [...nameFragments];
+nameFragments.push("MR");
+console.log(nameFragments, copiedNameFragments);
 console.log(Math.min(...prices));
 
-const persons = [{ name: 'Mr', age: 30 }, { name: 'zizo', age: 31 }];
+const persons = [
+  { name: "Mr", age: 30 },
+  { name: "zizo", age: 31 }
+];
 const copiedPersons = [...persons]; // copy address place of memory
 console.log(persons, copiedPersons);
-const copiedPersons = [...persons.map(person => ({
+const copiedPersons = [
+  ...persons.map(person => ({
     name: person.name,
     age: age,
     hobbies: [...person.hobbies]
-}))];
+  }))
+];
 
-persons.push({ name: 'Anna', age: 29 });
-persons[0].age = 31; //refelact for 2 array
+persons.push({ name: "Anna", age: 29 });
+persons[0].age = 31; //reflect for 2 array
 console.log(persons, copiedPersons);
-////////////////////////////////////////////////////////////////////  Understanding Array Destructuring
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Understanding Array Destructuring
 ////////////////////////////////////////
 
-const nameData = ['Max', 'Abdelaziz' ,'Mr' ,30];
+const nameData = ["Max", "Abdelaziz", "Mr", 30];
 const fname = nameData[0];
 const lname = nameData[1];
 
-const [firstName , LastName ,...otherInformation] = nameData;
-console.log(firstName, LastName);
-//////////////////////////////////////////////////////////////////
+const [firstName, LastName, ...otherInformation] = nameData;
+console.log(firstName, LastName ,otherInformation);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Maps & Sets - Overview
+///////////////////////////
+
+// Look Image Maps&sets
+// GO TO setmaps.js
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
--- 8-More on Arrays & Iterables/setsmaps.js
+++ 8-More on Arrays & Iterables/setsmaps.js
@@ -1,10 +1,11 @@
 //  Working with Sets
-// set tools to manage some uniqe values
+// set tools to manage some unique values
 //////////////////////////////////////////
 const ids = new Set();
 const ids = new Set([1, 2, 3]);
 console.log(ids);
-ids.add(2); // Not Added becouse value must be uniqe
+console.log(ids.has(1)); // true
+ids.add(2); // Not Added because value must be unique
 if (ids.has('hi')) {
     ids.delete('hi');
 }
@@ -14,9 +15,10 @@ for (const entry of ids.entries()) {
     console.log(entry);//    [1,1] [2,2] [3,3]
 }
 const names = new Set(["Hi", "from", "sets"]);
-// you can use values()insted of enteries() the value
+// you can use values() instead of entries() the value
 for (const entry of names.entries()) {
-    console.log(entry);//    [hi,hi] [from,from] [sets,sets]
+    console.log(entry);   // [hi,hi] [from,from] [sets,sets]
+    console.log(entry[0]);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -25,8 +27,10 @@ for (const entry of names.entries()) {
 
 const person1 = { name: 'Max' };
 const person2 = { name: 'Manuel' };
-const personData = new Map([[person1, [{ data: 'yerstday', price: 30 }]]]);
-personData.set(person2, [{ data: 'HWLLOO', price: 30 }]);
+const map1 = new Map([['key','some value']]);
+
+const personData = new Map([[person1, [{ data: 'yesterday', price: 30 }]]]);
+personData.set(person2, [{ data: 'HALLOO', price: 30 }]);
 console.log(personData);
 console.log(personData.get(person1));
 for (const entry of personData.entries()) {
@@ -43,12 +47,14 @@ for (const key of personData.keys()) {
 for (const value of personData.values()) {
     console.log(value);
 }
-//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Understanding WeakSet
 // Allow the garbage collection no code uses in set
-//////////////////////////
+/////////////////////////////////////////////////////
 let person = {name:'Max'};
 const persons = new WeakSet();
 persons.add(person);
 person = null;
 console.log(persons);
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
